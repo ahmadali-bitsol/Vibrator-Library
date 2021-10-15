2503238c3ceea982e3e64869b12414b0faf3e095
ahmadali-bitsol <ahmadaliofficial0@gmail.com>
2021-10-15 07:17:45 +0500

    add fragment

--- PCVibratorModule/src/main/java/com/bitsol/pcvibratormodule/VibrationFragment.kt
+++ PCVibratorModule/src/main/java/com/bitsol/pcvibratormodule/VibrationFragment.kt
@@ -5,16 +5,25 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.bitsol.pcvibratormodule.databinding.FragmentVibrationBinding
 
 
 class VibrationFragment : Fragment() {
 
+    private lateinit var binding : FragmentVibrationBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_vibration, container, false)
+    ): View {
+        binding = FragmentVibrationBinding.inflate(inflater)
+
+
+        binding.tv1.setOnClickListener {
+            binding.tv1.text = "Ani diya mazak a"
+        }
+
+        return binding.root
     }
 
 }
